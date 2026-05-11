717fa196d6b08a2f353715ce25988fa656f242a8
unknown <YY229@ccs.local>
2026-05-11 20:45:49 +0800

    Uploading first attemp

---
+++ src/app/_helpers/must-match.validator.ts
@@ -0,0 +1,27 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+// custom validator to check that two fields match
+export function MustMatch(controlName: string, matchingControlName: string): ValidatorFn {
+    return (group: AbstractControl): ValidationErrors | null => {
+        const control = group.get(controlName);
+        const matchingControl = group.get(matchingControlName);
+
+        if (!control || !matchingControl) {
+            return null;
+        }
+
+        // return if another validator has already found an error on the matchingControl
+        if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+            return null;
+        }
+
+        // set error on matchingControl if validation fails
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+
+        return null;
+    }
+}
